d7daa40652a2839777a2bf3b04d9ca6c359d2991
Duy Huynh <d.huynh68@gmail.com>
2015-07-08 10:28:47 -0400

    Add DataStructures

---
+++ DataStructures.py
@@ -0,0 +1,54 @@
+class Stack:
+
+	def __init__(self):
+		self.items = []
+
+		def isEmpty(self):
+			return self.items == []
+
+		def push(self, item):
+			self.items.append(item)
+
+		def pop(self):
+			return self.items.pop()
+
+		def peek(self):
+			return self.items[len(self.items)-1]
+
+		def size(self):
+			return len(self.items)
+
+class Queue:
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def enqueue(self, items):
+		self.items.insert(item)
+
+	def dequeue(self, items):
+		return self.items.pop()
+
+	def size(self):
+		return len(self.items)
+
+class Deque:
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def addFront(self, item):
+		self.items.append(item)
+
+	def addRear(self, item):
+		self.items.insert(0, item)
+
+	def removeFront(self):
+		return self.items.pop()
+
+	def removeRear(self):
+
